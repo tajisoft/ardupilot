f73f884342e3554309e097bfd8c26394a62a09a0
Randy Mackay <rmackay9@yahoo.com>
2020-10-13 14:28:58 +0900

    AP_Scripting: ahrs-source uses innovations

--- libraries/AP_Scripting/examples/ahrs-source.lua
+++ libraries/AP_Scripting/examples/ahrs-source.lua
@@ -4,15 +4,16 @@
 -- setup RCx_OPTION = 89 (EKF Pos Source), move to middle position to enable automatic source selection
 -- setup EK3_SRC_ parameters so that GPS is the primary source, Non-GPS (i.e. T265) is the secondary source
 -- configure a forward or downward facing lidar with a range of more than 5m
+--
+-- When the auxiliary switch is in middle position, automatic source selection uses these thresholds:
 -- SCR_USER1 holds the threshold for rangefinder altitude:
 --     if rangefinder distance >= SCR_USER1, source1 will be used
 --     if rangefinder distance < SCR_USER1, source2 will be used
--- SCR_USER2 holds the threshold for GPS satellites:
--- SCR_USER3 holds the threshold for GPS hdop:
---     if both GPS satellites >= SCR_USER2 and HDOP <= SCR_USER3, source1 will be used
+-- SCR_USER2 holds the threshold for GPS speed accuracy (around 0.3 is a good choice)
+-- SCR_USER3 holds the threshold for Non-GPS vertical speed innovation (about 0.5 may be a good choice)
+--     if both GPS speed accuracy <= SCR_USER2 and ExternalNav speed variance >= SCR_USER3, source1 will be used
 --     otherwise source2 will be used
 --
--- automatic source selection will occur when the auxiliary switch is in middle position
 --    GPS will be used when RangeFinder returns distance of >=8m for at least 3 seconds
 --    NonGPS will be used when RangeFinder returns distance of <8m for at least 3 seconds
 
@@ -53,34 +54,16 @@ function update()
     end
   end
 
-  -- check GPS status
-  local gps_sat_thresh = param:get('SCR_USER2')     -- SCR_USER2 holds GPS satellite count threshold
-  local gps_hdop_thresh = param:get('SCR_USER3')    -- SCR_USER3 holds GPS hdop threshold
-  local gps_speedaccuracy_thresh = param:get('SCR_USER4')  -- SCR_USER4 holds GPS speed accuracy threshold
-  if gps_sat_thresh and gps_hdop_thresh and gps_speedaccuracy_thresh then
-    -- satellite count vote (vote "-1" to move towards GPS, "+1" to move to Non-GPS)
-    local gps_sat_vote = 0
-    if (gps_sat_thresh > 0) then
-      if (gps:num_sats(0) < gps_sat_thresh) then
-        gps_sat_vote = 1
-        --gcs:send_text(0, "GPS Sat low (" .. tostring(gps:num_sats(0)) .. "<" .. tostring(gps_sat_thresh) .. ")")
-      else
-        gps_sat_vote = -1
-        --gcs:send_text(0, "GPS Sat high (" .. tostring(gps:num_sats(0)) .. ">" .. tostring(gps_sat_thresh) .. ")")
-      end
-    end
-    -- hdop check vote (vote "-1" to move towards GPS, "+1" to move to Non-GPS)
-    local gps_hdop_vote = 0
-    if (gps_hdop_thresh > 0) then
-      if (gps:get_hdop(0) > gps_hdop_thresh) then
-        gps_hdop_vote = 1
-        --gcs:send_text(0, "GPS Hdop high (" .. tostring(gps:get_hdop(0)) .. ">" .. tostring(gps_hdop_thresh) .. ")")
-      else
-        gps_hdop_vote = -1
-        --gcs:send_text(0, "GPS Hdop low (" .. tostring(gps:get_hdop(0)) .. "<" .. tostring(gps_hdop_thresh) .. ")")
-      end
-    end
-	-- speed accuracy check vote (vote "-1" to move towards GPS, "+1" to move to Non-GPS)
+  -- get innovations from external nav
+  local extnav_innov = Vector3f()
+  local extnav_var = Vector3f()
+  extnav_innov, extnav_var = ahrs:get_vel_innovations_and_variances_by_source(6)
+
+  -- check GPS and ExternalNav status
+  local gps_speedaccuracy_thresh = param:get('SCR_USER2')  -- SCR_USER2 holds GPS speed accuracy threshold
+  local extnav_innov_thresh = param:get('SCR_USER3')  -- SCR_USER3 holds Non-GPS vertical velocity innovation
+  if gps_speedaccuracy_thresh and extnav_innov_thresh then
+    -- speed accuracy check vote (vote "-1" to move towards GPS, "+1" to move to Non-GPS)
     local gps_speedaccuracy_vote = 0
     if (gps_speedaccuracy_thresh > 0 and gps:speed_accuracy(0)) then
       if (gps:speed_accuracy(0) > gps_speedaccuracy_thresh) then
@@ -91,35 +74,31 @@ function update()
         --gcs:send_text(0, "GPS SpdAcc low (" .. tostring(gps:speed_accuracy(0)) .. "<" .. tostring(gps_speedaccuracy_thresh) .. ")")
       end
     end
+    -- Non-GPS innovation check vote (vote "-1" to move towards GPS, "+1" to move to Non-GPS)
+    local extnav_innov_vote = 0
+    if (extnav_innov and extnav_innov_thresh > 0) then
+      if (math.abs(extnav_innov:z()) > extnav_innov_thresh) then
+        extnav_innov_vote = -1
+        --gcs:send_text(0, "ExtNav Innov high (" .. tostring(math.abs(extnav_innov:z())) .. ">" .. tostring(extnav_innov_thresh) .. ")")
+      else
+        --gcs:send_text(0, "ExtNav Innov OK (" .. tostring(math.abs(extnav_innov:z())) .. "<=" .. tostring(extnav_innov_thresh) .. ")")
+      end
+    end
     -- combine votes (a vote of "0" means no vote)
-    if gps_sat_vote == 0 then
-      if gps_hdop_vote ~= 0 then
-        gps_sat_vote = gps_hdop_vote
-      else
-        gps_sat_vote = gps_speedaccuracy_vote
-      end
-    end
-    if gps_hdop_vote == 0 then
-      if gps_sat_vote ~= 0 then
-        gps_hdop_vote = gps_sat_vote
-      else
-        gps_hdop_vote = gps_speedaccuracy_vote
-      end
-    end
     if gps_speedaccuracy_vote == 0 then
-      if gps_sat_vote ~= 0 then
-        gps_speedaccuracy_vote = gps_sat_vote
-      else
-        gps_speedaccuracy_vote = gps_hdop_vote
-      end
+      gps_speedaccuracy_vote = extnav_innov_vote
     end
-    if (gps_sat_vote > 0 or gps_hdop_vote > 0 or gps_speedaccuracy_vote > 0) then
-      -- satellite count, hdop or speed accuracy is not good so move towards Non-GPS
+    if extnav_innov_vote == 0 then
+      extnav_innov_vote = gps_speedaccuracy_vote
+    end
+
+    if (gps_speedaccuracy_vote > 0 and extnav_innov_vote >= 0) then
+      -- gps speed accuracy is not good and external nav is OK so move towards Non-GPS
       source_counter = math.max(source_counter, 0)
       source_counter = source_counter + 1
       source_counter = math.min(source_counter, source_counter_max)
-    elseif (gps_sat_vote < 0 and gps_hdop_vote < 0 and gps_speedaccuracy_vote < 0) then
-      -- satellite count, hdop and speed accuracy are good (or unchecked) so move towards GPS
+    elseif (gps_speedaccuracy_vote < 0 or extnav_innov_vote < 0) then
+      -- gps speed accuracy is good (or unchecked) or extnav innovations are bad so move towards GPS
       source_counter = math.min(source_counter, 0)
       source_counter = source_counter - 1
       source_counter = math.max(source_counter, -source_counter_max)
